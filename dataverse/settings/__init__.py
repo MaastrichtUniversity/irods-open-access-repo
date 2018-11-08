13b1d559c4ac2078db65b6effccf0aba298a44ef
JonathanMELIUS <meliusjonathan@gmail.com>
2018-11-08 16:10:00 +0100

    clean up

--- dataverse/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-from dataverse.settings.defaults import *  # noqa
-
-try:
-    from dataverse.settings.local import *  # noqa
-except ImportError as error:
-    pass
