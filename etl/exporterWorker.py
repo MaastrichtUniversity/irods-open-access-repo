d1289d1bdd63457fea4b6241dd090bc19481aaec
JonathanMELIUS <meliusjonathan@gmail.com>
2021-06-04 14:21:17 +0200

    Update extend_folder_path function [DHS-1149, DHS-1559]

--- etl/exporterWorker.py
+++ etl/exporterWorker.py
@@ -19,6 +19,33 @@ logging.basicConfig(level=logging.getLevelName(log_level), format='%(asctime)s %
 logger = logging.getLogger('root')
 
 
+def extend_folder_path(session, selected_list):
+    """
+    If a folder is part of the original selected list, this function will recursively walk into it to retrieve all
+    its files children and add them to the original selected list
+    """
+    extended_list = ""
+    for path in selected_list.split(","):
+        absolute_path = "/nlmumc/projects/" + path
+        # Check if the path is collection
+        if session.collections.exists(absolute_path):
+            collection = session.collections.get(absolute_path)
+            for coll, sub, files in collection.walk():
+                for file in files:
+                    if extended_list == "":
+                        extended_list = file.path.replace("/nlmumc/projects/", "")
+                    else:
+                        extended_list += ","+file.path.replace("/nlmumc/projects/", "")
+        # Or a file
+        else:
+            if extended_list == "":
+                extended_list = path
+            else:
+                extended_list += ","+path
+
+    return extended_list
+
+
 def collection_etl(ch, method, properties, body):
     try:
         data = ast.literal_eval(body.decode("utf-8"))
@@ -33,6 +60,7 @@ def collection_etl(ch, method, properties, body):
         logger.info(f" [x] Create {data['repository']} exporter worker")
         class_name = data['repository'] + 'Exporter'
         exporter = globals()[class_name]()
+        data['restrict_list'] = extend_folder_path(irods_client.session, data['restrict_list'])
         exporter.init_export(irods_client, data)
         ch.basic_ack(delivery_tag=method.delivery_tag)
         logger.info(" [x] Sent projectCollection.exporter.executed")
