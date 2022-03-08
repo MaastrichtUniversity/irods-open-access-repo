c0ae661826336b9bd4be69ef37745d864e882228
JonathanMELIUS <meliusjonathan@gmail.com>
2022-03-08 11:37:44 +0100

    Add checks for empty values during mapping [DHS-1829, DHS-2155]

--- etl/dataverseManager/dataverseMetadataMapper.py
+++ etl/dataverseManager/dataverseMetadataMapper.py
@@ -2,17 +2,12 @@ import json
 import logging
 import urllib.request
 
-logger = logging.getLogger('iRODS to Dataverse')
-
-'''
-TODO
-self.protocol = None
- '''
+logger = logging.getLogger("iRODS to Dataverse")
 
 
 class MetadataMapper:
-    """Map iRODS metadata to the Open Access Repository metadata format
-    """
+    """Map iRODS metadata to the Open Access Repository metadata format"""
+
     def __init__(self, collection_avu, depositor, instance):
         self.collection_avu = collection_avu
         self.dataset_json = None
@@ -27,18 +22,17 @@ class MetadataMapper:
         self.get_controlled_vocabulary()
         logger.info("--\t Map metadata")
 
-        with open('etl/resources/template.json') as f:
+        with open("etl/resources/template.json") as f:
             self.dataset_json = json.load(f)
 
-        self.md = self.dataset_json['datasetVersion']
+        self.md = self.dataset_json["datasetVersion"]
 
         self.add_author(self.instance.creator.full_name)
 
         if self.instance.identifier.pid is None:
             logger.error(f"{'--':<20}PID invalid")
         else:
-            url = f"https://hdl.handle.net/{self.instance.identifier.pid}"
-            self.add_alternative_url(url)
+            self.add_alternative_url(self.instance.identifier.pid)
 
         if self.instance.description.description is None:
             self.add_description("")
@@ -51,51 +45,14 @@ class MetadataMapper:
 
         self.add_subject()
 
-        depositor = self.depositor.split("@")[0]
-        self.add_depositor(depositor)
+        self.add_depositor(self.depositor.split("@")[0])
 
-        contacts = []
-        for contact in self.instance.contacts.contacts:
-            pub = self.add_contact(contact.full_name, contact.email, contact.affiliation.label)
-            contacts.append(pub)
-        self.add_contacts(contacts)
+        self.map_contacts()
+        self.map_contributors()
+        self.map_keywords()
+        self.map_publications()
 
-        contributors = []
-        for contributor in self.instance.contributors.contributors:
-            contributor_type = ""
-            if contributor.type.label in self.contributor_type_vocabulary:
-                contributor_type = self.contributor_type_vocabulary[contributor.type.label]
-
-            pub = self.add_contributor(contributor.full_name, contributor_type)
-            contributors.append(pub)
-        self.add_contributors(contributors)
-
-        keywords = []
-        for subject in self.instance.subjects.subjects:
-            if subject.scheme_iri is not None:
-                keyword = self.add_keyword(subject.keyword, subject.scheme_iri.rsplit("/", 1)[1], subject.value_uri.uri)
-            else:
-                keyword = self.add_keyword(subject.keyword, "", "")
-            keywords.append(keyword)
-        self.add_keywords(keywords)
-
-        publications = []
-        for resource in self.instance.related_resources.related_resources:
-            value = resource.identifier
-            url = ""
-            if resource.identifier.startswith("http"):
-                url = resource.identifier
-                value = resource.identifier.rsplit("/", 1)[1]
-
-            publication_id_type = ""
-            if resource.identifier_type.label.lower() in self.publication_id_type_vocabulary:
-                publication_id_type = self.publication_id_type_vocabulary[resource.identifier_type.label.lower()]
-
-            pub = self.add_publication(value, publication_id_type, url)
-            publications.append(pub)
-        self.add_publications(publications)
-
-        self.dataset_json['datasetVersion'] = self.md
+        self.dataset_json["datasetVersion"] = self.md
         logger.debug(json.dumps(self.dataset_json, indent=4))
 
         return self.dataset_json
@@ -103,6 +60,58 @@ class MetadataMapper:
     def update_fields(self, new):
         fields = self.md["metadataBlocks"]["citation"]["fields"]
         fields.append(new)
+
+    def map_contacts(self):
+        contacts = []
+        for contact in self.instance.contacts.contacts:
+            contacts.append(self.add_contact(contact.full_name, contact.email, contact.affiliation.label))
+        self.add_contacts(contacts)
+
+    def map_contributors(self):
+        contributors = []
+        for contributor in self.instance.contributors.contributors:
+            # Map contributor_type to Dataverse controlled vocabulary for contributor type
+            contributor_type = ""
+            if contributor.type.label in self.contributor_type_vocabulary:
+                contributor_type = self.contributor_type_vocabulary[contributor.type.label]
+
+            contributors.append(self.add_contributor(contributor.full_name, contributor_type))
+        self.add_contributors(contributors)
+
+    def map_keywords(self):
+        keywords = []
+        for subject in self.instance.subjects.subjects:
+            # Case where a subject has no free-text or ontology value
+            if subject.scheme_iri is None and subject.keyword is None:
+                continue
+            if subject.scheme_iri is not None:
+                keyword = self.add_keyword(subject.keyword, subject.scheme_iri.rsplit("/", 1)[1], subject.value_uri.uri)
+            else:
+                keyword = self.add_keyword(subject.keyword, "", "")
+            keywords.append(keyword)
+        self.add_keywords(keywords)
+
+    def map_publications(self):
+        publications = []
+        for resource in self.instance.related_resources.related_resources:
+            # Case where a related_resource has no identifier
+            if resource.identifier is None:
+                continue
+
+            # Check if the resource identifier is a URI or not
+            value = resource.identifier
+            url = ""
+            if value and value.startswith("http"):
+                url = value
+                value = value.rsplit("/", 1)[1]
+
+            # Map publication_id_type to Dataverse controlled vocabulary for publication id type
+            publication_id_type = ""
+            if resource.identifier_type.label.lower() in self.publication_id_type_vocabulary:
+                publication_id_type = self.publication_id_type_vocabulary[resource.identifier_type.label.lower()]
+
+            publications.append(self.add_publication(value, publication_id_type, url))
+        self.add_publications(publications)
 
     def add_author(self, author, affiliation="", up=True):
         new = {
@@ -114,29 +123,24 @@ class MetadataMapper:
                         "typeName": "authorAffiliation",
                         "multiple": False,
                         "value": affiliation,
-                        "typeClass": "primitive"
+                        "typeClass": "primitive",
                     },
                     "authorName": {
                         "typeName": "authorName",
                         "multiple": False,
                         "value": author,
-                        "typeClass": "primitive"
-                    }
+                        "typeClass": "primitive",
+                    },
                 }
             ],
-            "typeClass": "compound"
+            "typeClass": "compound",
         }
         if up:
             self.update_fields(new)
         return new
 
     def add_title(self, title, up=True):
-        new = {
-            "typeName": "title",
-            "multiple": False,
-            "value": title,
-            "typeClass": "primitive"
-        }
+        new = {"typeName": "title", "multiple": False, "value": title, "typeClass": "primitive"}
         if up:
             self.update_fields(new)
         return new
@@ -151,11 +155,11 @@ class MetadataMapper:
                         "typeName": "dsDescriptionValue",
                         "multiple": False,
                         "value": description,
-                        "typeClass": "primitive"
+                        "typeClass": "primitive",
                     }
                 }
             ],
-            "typeClass": "compound"
+            "typeClass": "compound",
         }
         if up:
             self.update_fields(new)
@@ -165,44 +169,27 @@ class MetadataMapper:
         new = {
             "typeName": "subject",
             "multiple": True,
-            "value": [
-                "Medicine, Health and Life Sciences"
-            ],
-            "typeClass": "controlledVocabulary"
+            "value": ["Medicine, Health and Life Sciences"],
+            "typeClass": "controlledVocabulary",
         }
         if up:
             self.update_fields(new)
         return new
 
     def add_date(self, date, up=True):
-        new = {
-            "typeName": "dateOfDeposit",
-            "multiple": False,
-            "value": date,
-            "typeClass": "primitive"
-        }
+        new = {"typeName": "dateOfDeposit", "multiple": False, "value": date, "typeClass": "primitive"}
         if up:
             self.update_fields(new)
         return new
 
     def add_depositor(self, depositor, up=True):
-        new = {
-            "typeName": "depositor",
-            "multiple": False,
-            "value": depositor,
-            "typeClass": "primitive"
-        }
+        new = {"typeName": "depositor", "multiple": False, "value": depositor, "typeClass": "primitive"}
         if up:
             self.update_fields(new)
         return new
 
     def add_contacts(self, contacts, up=True):
-        new = {
-            "typeName": "datasetContact",
-            "multiple": True,
-            "value": contacts,
-            "typeClass": "compound"
-        }
+        new = {"typeName": "datasetContact", "multiple": True, "value": contacts, "typeClass": "compound"}
         if up:
             self.update_fields(new)
         return new
@@ -214,7 +201,7 @@ class MetadataMapper:
                 "typeName": "datasetContactEmail",
                 "multiple": False,
                 "value": email,
-                "typeClass": "primitive"
+                "typeClass": "primitive",
             }
         }
         return new
@@ -226,30 +213,25 @@ class MetadataMapper:
                 "multiple": False,
                 "typeClass": "primitive",
                 "typeName": "datasetContactAffiliation",
-                "value": affiliation
+                "value": affiliation,
             },
             "datasetContactEmail": {
                 "multiple": False,
                 "typeClass": "primitive",
                 "typeName": "datasetContactEmail",
-                "value": email
+                "value": email,
             },
             "datasetContactName": {
                 "multiple": False,
                 "typeClass": "primitive",
                 "typeName": "datasetContactName",
-                "value": name
-            }
+                "value": name,
+            },
         }
         return new
 
     def add_keywords(self, keywords, up=True):
-        new = {
-            "multiple": True,
-            "typeClass": "compound",
-            "typeName": "keyword",
-            "value": keywords
-        }
+        new = {"multiple": True, "typeClass": "compound", "typeName": "keyword", "value": keywords}
         if up:
             self.update_fields(new)
         return new
@@ -257,100 +239,79 @@ class MetadataMapper:
     @staticmethod
     def add_keyword(value, vocabulary, uri):
         new = {
-            "keywordValue": {
-                "multiple": False,
-                "typeClass": "primitive",
-                "typeName": "keywordValue",
-                "value": value
-            },
+            "keywordValue": {"multiple": False, "typeClass": "primitive", "typeName": "keywordValue", "value": value},
             "keywordVocabulary": {
                 "multiple": False,
                 "typeClass": "primitive",
                 "typeName": "keywordVocabulary",
-                "value": vocabulary
+                "value": vocabulary,
             },
             "keywordVocabularyURI": {
                 "multiple": False,
                 "typeClass": "primitive",
                 "typeName": "keywordVocabularyURI",
-                "value": uri
-            }
+                "value": uri,
+            },
         }
         return new
 
     def add_alternative_url(self, url, up=True):
-        new = {
-            "multiple": False,
-            "typeClass": "primitive",
-            "typeName": "alternativeURL",
-            "value": url
-        }
+        new = {"multiple": False, "typeClass": "primitive", "typeName": "alternativeURL", "value": url}
         if up:
             self.update_fields(new)
         return new
 
     def add_publications(self, publications, up=True):
-        new = {
-            "multiple": True,
-            "typeClass": "compound",
-            "typeName": "publication",
-            "value": publications
-        }
+        new = {"multiple": True, "typeClass": "compound", "typeName": "publication", "value": publications}
         if up:
             self.update_fields(new)
         return new
 
     @staticmethod
-    def add_publication(value, doi, url):
+    def add_publication(value, publication_id_type, url):
         new = {
             "publicationIDNumber": {
                 "multiple": False,
                 "typeClass": "primitive",
                 "typeName": "publicationIDNumber",
-                "value": value
+                "value": value,
             },
-            "publicationIDType": {
+            "publicationURL": {"multiple": False, "typeClass": "primitive", "typeName": "publicationURL", "value": url},
+        }
+
+        if publication_id_type:
+            new["publicationIDType"] = {
                 "multiple": False,
                 "typeClass": "controlledVocabulary",
                 "typeName": "publicationIDType",
-                "value": doi
-            },
-            "publicationURL": {
-                "multiple": False,
-                "typeClass": "primitive",
-                "typeName": "publicationURL",
-                "value": url
+                "value": publication_id_type,
             }
-        }
         return new
 
     def add_contributors(self, contributors, up=True):
-        new = {
-            "multiple": True,
-            "typeClass": "compound",
-            "typeName": "contributor",
-            "value": contributors
-        }
+        new = {"multiple": True, "typeClass": "compound", "typeName": "contributor", "value": contributors}
         if up:
             self.update_fields(new)
         return new
 
     @staticmethod
-    def add_contributor(name, role):
+    def add_contributor(name, contributor_type):
         new = {
-            "contributorType": {
-                "typeName": "contributorType",
-                "multiple": False,
-                "typeClass": "controlledVocabulary",
-                "value": role
-            },
             "contributorName": {
                 "typeName": "contributorName",
                 "multiple": False,
                 "typeClass": "primitive",
-                "value": name
+                "value": name,
             }
         }
+        if contributor_type:
+            new["contributorType"] = {
+                "typeName": "contributorType",
+                "multiple": False,
+                "typeClass": "controlledVocabulary",
+                "value": contributor_type,
+            }
+
         return new
 
     def get_controlled_vocabulary(self):
