40a93ae62399f73545261dbcd96871cb005cb347
JonathanMELIUS <meliusjonathan@gmail.com>
2022-03-08 11:36:16 +0100

    Update update_metadata_status calls [DHS-1829, DHS-2155]

--- etl/dataverseManager/dataverseClient.py
+++ etl/dataverseManager/dataverseClient.py
@@ -15,12 +15,11 @@ from requests.exceptions import ProxyError
 from http import HTTPStatus
 from multiprocessing import Pool
 
-logger = logging.getLogger('iRODS to Dataverse')
+logger = logging.getLogger("iRODS to Dataverse")
 
 
 class DataverseClient(ExporterClient):
-    """Dataverse client to import datasets and files
-    """
+    """Dataverse client to import datasets and files"""
 
     def __init__(self, host, token, alias, irodsclient):
         """
@@ -57,44 +56,42 @@ class DataverseClient(ExporterClient):
     def create_dataset(self, md, data_export=False):
         logger.info(f"{'--':<10}Dataset - request creation")
 
-        self.irods_client.update_metadata_status(Status.CREATE_EXPORTER.value, Status.CREATE_DATASET.value)
+        self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.CREATE_DATASET.value)
         url = f"{self.host}/api/dataverses/{self.alias}/datasets/"
 
         try:
             resp = requests.post(
                 url,
                 data=json.dumps(md),
-                headers={'Content-type': 'application/json',
-                         'X-Dataverse-key': self.token
-                         },
+                headers={"Content-type": "application/json", "X-Dataverse-key": self.token},
             )
             if resp.status_code == HTTPStatus.CREATED.value:
-                self.dataset_pid = resp.json()['data']['persistentId']
+                self.dataset_pid = resp.json()["data"]["persistentId"]
                 self.dataset_url = f"{self.host}/dataset.xhtml?persistentId={self.dataset_pid}&version=DRAFT"
                 self.dataset_deposit_url = f"{self.host}/api/datasets/:persistentId/add?persistentId={self.dataset_pid}"
                 logger.info(f"{'--':<20}Dataset created with pid: {self.dataset_pid}")
             else:
                 logger.error(f"{'--':<20}Create dataset failed")
                 logger.error(resp.content)
-                self.irods_client.update_metadata_status(Status.CREATE_DATASET.value, Status.CREATE_DATASET_FAILED.value)
+                self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.CREATE_DATASET_FAILED.value)
 
             if not data_export and resp.status_code == HTTPStatus.CREATED.value:
-                self.irods_client.update_metadata_status(Status.CREATE_DATASET.value, Status.FINALIZE.value)
+                self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.FINALIZE.value)
                 self._final_report()
                 self.email_confirmation()
                 self.submit_dataset_for_review()
         except ProxyError:
             logger.error(self.host + " cannot be reached. Create dataset failed")
-            self.irods_client.update_metadata_status(Status.CREATE_DATASET.value, Status.CREATE_DATASET_FAILED.value)
+            self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.CREATE_DATASET_FAILED.value)
 
-    def import_files(self, deletion=False, restrict=False, restrict_list=''):
+    def import_files(self, deletion=False, restrict=False, restrict_list=""):
         self.deletion = deletion
         self.restrict = restrict
         if len(restrict_list) > 0:
             self.restrict_list = restrict_list
 
         if self.dataset_deposit_url is not None:
-            self.irods_client.update_metadata_status(Status.CREATE_DATASET.value, Status.PREPARE_COLLECTION.value)
+            self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.PREPARE_COLLECTION.value)
             self.pool = Pool(processes=1)
             self.pool_result = self.pool.apply_async(self.run_checksum, [self.irods_client.coll.path])
 
@@ -111,12 +108,12 @@ class DataverseClient(ExporterClient):
                 self.submit_dataset_for_review()
         else:
             logger.error(f"{'--':<20}Dataset unknown")
-            self.irods_client.update_metadata_status(Status.CREATE_DATASET.value, Status.DATASET_UNKNOWN.value)
+            self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.DATASET_UNKNOWN.value)
 
     def _prepare_zip(self):
         logger.info(f"{'--':<10}Prepare zip")
 
-        self.irods_client.update_metadata_status(Status.PREPARE_COLLECTION.value, Status.ZIP_COLLECTION.value)
+        self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.ZIP_COLLECTION.value)
         irods_md5 = hashlib.md5()
         size_bundle = zip_generator_faker(self.irods_client, self.upload_success, irods_md5, self.restrict_list)
         md5_hexdigest = irods_md5.hexdigest()
@@ -127,34 +124,31 @@ class DataverseClient(ExporterClient):
     def _upload_zip_collection(self, size_bundle):
         logger.info(f"{'--':<10}Upload zip")
 
-        self.irods_client.update_metadata_status(Status.ZIP_COLLECTION.value, Status.UPLOAD_ZIPPED_COLLECTION.value)
+        self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.UPLOAD_ZIPPED_COLLECTION.value)
         self.irods_md5 = hashlib.md5()
-        bundle_iterator = get_zip_generator(self.irods_client, self.upload_success,
-                                            self.irods_md5, self.restrict_list, size_bundle)
+        bundle_iterator = get_zip_generator(
+            self.irods_client, self.upload_success, self.irods_md5, self.restrict_list, size_bundle
+        )
         json_data = {"restrict": self.restrict}
         multipart_encoder = MultipartEncoder(
-            fields={'jsonData': json.dumps(json_data),
-                    'file': (self.zip_name, bundle_iterator)
-                    }
+            fields={"jsonData": json.dumps(json_data), "file": (self.zip_name, bundle_iterator)}
         )
         try:
             resp = requests.post(
                 self.dataset_deposit_url,
                 data=multipart_encoder,
-                headers={'Content-Type': multipart_encoder.content_type,
-                         'X-Dataverse-key': self.token
-                         },
+                headers={"Content-Type": multipart_encoder.content_type, "X-Dataverse-key": self.token},
             )
         except ProxyError:
             logger.error(self.host + " cannot be reached. Upload data failed")
-            self.irods_client.update_metadata_status(Status.UPLOAD_ZIPPED_COLLECTION.value, Status.UPLOAD_FAILED.value)
+            self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.UPLOAD_FAILED.value)
 
         return resp
 
     def _validate_checksum(self):
         logger.info(f"{'--':<10}Validate checksum")
 
-        self.irods_client.update_metadata_status(Status.UPLOAD_ZIPPED_COLLECTION.value, Status.VALIDATE_CHECKSUM.value)
+        self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.VALIDATE_CHECKSUM.value)
         self.pool.close()
         self.pool.join()
         chksums = self.pool_result.get()
@@ -184,48 +178,54 @@ class DataverseClient(ExporterClient):
             logger.info(f"{'--':<20}iRODS & buffer SHA-256 checksum: validated")
         else:
             logger.error(f"{'--':<20}SHA-256 checksum: failed")
-            self.irods_client.update_metadata_status(Status.VALIDATE_UPLOAD.value, Status.UPLOAD_CORRUPTED.value)
+            self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.UPLOAD_CORRUPTED.value)
 
         return validated
 
     def _validate_upload(self, resp):
         logger.info(f"{'--':<10}Validate upload")
 
-        self.irods_client.update_metadata_status(Status.VALIDATE_CHECKSUM.value, Status.VALIDATE_UPLOAD.value)
+        self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.VALIDATE_UPLOAD.value)
         count = 0
         validated = False
         if resp.status_code == HTTPStatus.OK.value:
-            for file_json in resp.json()['data']['files']:
+            for file_json in resp.json()["data"]["files"]:
                 # Check if the file is at the root of the collection
-                if 'directoryLabel' in file_json:
-                    file_path = self.irods_client.coll.path + "/" + file_json['directoryLabel'] + "/" + file_json['dataFile']['filename']
+                if "directoryLabel" in file_json:
+                    file_path = (
+                        self.irods_client.coll.path
+                        + "/"
+                        + file_json["directoryLabel"]
+                        + "/"
+                        + file_json["dataFile"]["filename"]
+                    )
                 else:
-                    file_path = self.irods_client.coll.path + "/" + file_json['dataFile']['filename']
+                    file_path = self.irods_client.coll.path + "/" + file_json["dataFile"]["filename"]
                 # index 1 -> md5_hexdigest
-                if file_json['dataFile']['md5'] == self.upload_success[file_path][1]:
+                if file_json["dataFile"]["md5"] == self.upload_success[file_path][1]:
                     count += 1
             if count == len(self.upload_success):
                 validated = True
                 logger.info(f"{'--':<20}iRODS & Dataverse MD5 checksum: validated")
-                self.irods_client.update_metadata_status(Status.VALIDATE_UPLOAD.value, Status.FINALIZE.value)
+                self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.FINALIZE.value)
         else:
             logger.error(f"{'--':<30}{resp.content.decode('utf-8')}")
-            self.irods_client.update_metadata_status(Status.VALIDATE_UPLOAD.value, Status.UPLOAD_FAILED.value)
+            self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.UPLOAD_FAILED.value)
 
         return validated
 
     def _final_report(self):
         logger.info(f"{'--':<10}Report final progress")
-        self.irods_client.add_metadata('externalPID', self.dataset_pid, "Dataverse")
-        self.irods_client.update_metadata_status(Status.FINALIZE.value, Status.EXPORTED.value)
+        self.irods_client.add_metadata("externalPID", self.dataset_pid, "Dataverse")
+        self.irods_client.update_metadata_status(Status.ATTRIBUTE.value, Status.EXPORTED.value)
         time.sleep(5)
         self.irods_client.remove_metadata(Status.ATTRIBUTE.value, f"Dataverse:{Status.EXPORTED.value}")
         logger.info(f"{'--':<10}Export Done")
 
     def email_confirmation(self):
-        host = os.environ['DH_MAILER_HOST']
-        user = os.environ['DH_MAILER_USERNAME']
-        pwd = os.environ['DH_MAILER_PASSWORD']
+        host = os.environ["DH_MAILER_HOST"]
+        user = os.environ["DH_MAILER_USERNAME"]
+        pwd = os.environ["DH_MAILER_PASSWORD"]
         from_address = "datahub@maastrichtuniversity.nl"
 
         endpoint = "http://" + host + "/email/send"
@@ -243,10 +243,9 @@ class DataverseClient(ExporterClient):
             "PID": self.irods_client.imetadata.pid,
             "TIMESTAMP": time.strftime("%d-%m-%Y %H:%M:%S"),
             "DEPOSITOR": self.irods_client.imetadata.depositor,
-
             "REPOSITORY": self.repository,
             "EXTERNAL_PID": self.dataset_pid,
-            "DATASET_URL": self.dataset_url
+            "DATASET_URL": self.dataset_url,
         }
 
         data_user = {
@@ -256,7 +255,7 @@ class DataverseClient(ExporterClient):
             "emailOptions": {
                 "from": from_address,
                 "to": depositor_email,
-            }
+            },
         }
 
         # Post the e-mail confirmation to the user
@@ -279,9 +278,7 @@ class DataverseClient(ExporterClient):
         try:
             resp = requests.post(
                 url,
-                headers={'Content-type': 'application/json',
-                         'X-Dataverse-key': self.token
-                         },
+                headers={"Content-type": "application/json", "X-Dataverse-key": self.token},
             )
             if resp.status_code == HTTPStatus.OK.value:
                 logger.info(f"Dataset have been submitted for review: {self.dataset_url}")
@@ -291,4 +288,4 @@ class DataverseClient(ExporterClient):
                 logger.error(resp.content)
         except ProxyError:
             logger.error(self.host + " cannot be reached. Submit for review failed")
-            self.irods_client.add_metadata(Status.ATTRIBUTE.value,  f"Dataverse:{Status.REQUEST_REVIEW_FAILED}")
+            self.irods_client.add_metadata(Status.ATTRIBUTE.value, f"Dataverse:{Status.REQUEST_REVIEW_FAILED}")
