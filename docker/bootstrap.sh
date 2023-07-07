e4e685510c4eefb8b23fe3f6dffe369c59e65b29
danieltheunissen <d.theunissen@maastrichtuniversity.nl>
2023-07-07 11:19:06 +0200

    Add help center to containers /etc/hosts file [DHS-3178]

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
- 
+
+ # Update /etc/hosts file with help center url pointing to proxy ip address so it can be resolved in the container (only for dev)
+if [ "$ENV" == "local" ]; then
+    echo "172.21.1.100 help.mdr.${ENV}.dh.unimaas.nl" >> /etc/hosts
+fi
+
 # End with exec call to preserve signals
 exec python3 -u /opt/app/etl/exporterWorker.py
